c14661034543f51e2257c5b122c3b61669fb5ba3
rkhab <marr@axp.tk>
2019-06-21 03:36:35 +0300

    init

---
+++ precache-manifest.7bdb4afc0536824fa35a13d1cd7f23c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6898e4351e3a83bdf055526dab164bbf",
+    "url": "/resistance/static/media/anonymous.6898e435.svg"
+  },
+  {
+    "revision": "d0b0206be6629f432043",
+    "url": "/resistance/static/css/main.39ed4ee5.chunk.css"
+  },
+  {
+    "revision": "c8d3745398ecb11135d6",
+    "url": "/resistance/static/js/runtime~main.b4ba2b66.js"
+  },
+  {
+    "revision": "01b73c9627b3c1a491b4",
+    "url": "/resistance/static/js/2.4e428546.chunk.js"
+  },
+  {
+    "revision": "f4720874047296f35cb17e98a2a303c6",
+    "url": "/resistance/static/media/secret-agent.f4720874.svg"
+  },
+  {
+    "revision": "1cc063fe1e5834e18e024adb6c40f41c",
+    "url": "/resistance/static/media/soldier.1cc063fe.svg"
+  },
+  {
+    "revision": "d0b0206be6629f432043",
+    "url": "/resistance/static/js/main.b2e03b85.chunk.js"
+  },
+  {
+    "revision": "fc7580696b2175b59231c7a9924487ff",
+    "url": "/resistance/static/media/captain.fc758069.svg"
+  },
+  {
+    "revision": "09706776f7146fe352638c7eee3f15a6",
+    "url": "/resistance/static/media/captain-red.09706776.svg"
+  },
+  {
+    "revision": "9ff8d9ba0c328cccb4d4c75ea24c217f",
+    "url": "/resistance/static/media/ninja.9ff8d9ba.svg"
+  },
+  {
+    "revision": "fb712ee8ef13d1a93187853fecb9ac18",
+    "url": "/resistance/static/media/bodyguard.fb712ee8.svg"
+  },
+  {
+    "revision": "ff13475308fd5f3ab58841db1082bb36",
+    "url": "/resistance/index.html"
+  }
+];
